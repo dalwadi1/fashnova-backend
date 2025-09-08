d8b66d5b5ec0462161fc511ff376bb868dea65f0
Rutvik Parmar <parmarrutvik1807@gmail.com>
2025-09-08 15:27:16 +0530

    add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import router from "./routes/router.js";
@@ -6,7 +7,7 @@ import router from "./routes/router.js";
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
